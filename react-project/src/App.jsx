d63d9809380cba5bf7d421d54c80ac703eae5b64
Alisher <alisherxujanov163@gmail.com>
2024-07-30 17:40:54 +0500

    update

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import FirstComp from './components/FirstComp';
+import SecondComp from './components/SecondComp';
 import ThirdComp from './components/ThirdComp';
 
 
 export default function App() {
   return (
     <div>
-      <FirstComp/>
-      <SecondComp/>
-     <ThirdComp/>
+      <FirstComp />
+      <SecondComp />
+      <ThirdComp />
     </div>
   )
 }
--- react-project/src/components/SecondComp.jsx
+++ react-project/src/components/SecondComp.jsx
@@ -1,30 +1,45 @@
-
+import { useState } from 'react'
 
 export default function SecondComp() {
 
-    const [name,setName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [message,setMessage] = useState("")
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+  const [message, setMessage] = useState("")
 
-    function handleSubmit(e){
-        e.preventDefault()
-        console.log({name,email,password})
-    }
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(form)
+  }
+
+  function updateForm(e) {
+    const name = e.target.name
+    setForm({ ...form, [name]: e.target.value })
+  }
+
   return (
     <div>
-      <form id='fr'
-       onSubmit={handleSubmit}>
-        <label htmlFor="username">username</label>
-       <input onChange={(e) => setName(e.target.value)}
-       type="text" placeholder='enter username'/>
-       <label htmlFor="email">email</label>
-       <input onChange={(e) => setEmail(e.target.value)}
-       type="text" placeholder='enter email' />
-       <label htmlFor="password">password</label>
-       <input onChange={(e) => setPassword(e.target.value)}
-       type="text" placeholder='enter password' />
-    <button>submit</button>
+      <form id='form' onSubmit={handleSubmit}>
+
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input onChange={updateForm} type="text" name='username' placeholder='Enter username' />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input onChange={updateForm} type="email" name='email' placeholder='Enter email' />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input onChange={updateForm} type="password" name='password' placeholder='Enter password' />
+        </div>
+
+        <div className="form-control">
+          <button>submit</button>
+        </div>
       </form>
     </div>
   )
