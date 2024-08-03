acbe789e6634bf3f128cdf2115caf1bd7b412af8
Alisher <alisherxujanov163@gmail.com>
2024-08-03 18:23:54 +0500

    counter

--- react-project/src/components/Contacts/index.jsx
+++ react-project/src/components/Contacts/index.jsx
@@ -1,9 +1,24 @@
 import './style.scss'
+import { useState } from 'react';
 
 function Contacts() {
+    const [count, setCount] = useState(0);
+
     return (
         <main className="contacts-page-wrapper">
             <h1>Contacts Page</h1>
+
+
+            <hr />
+            <br />
+            <h2>{count}</h2>
+            <br />
+            <button className='warning-btn' onClick={(e) => { setCount(count + 1) }} >
+                Increment
+            </button>
+            <button className='warning-btn' onClick={(e) => { setCount(count - 1) }} >
+                Decrement
+            </button>
         </main>
     );
 }
