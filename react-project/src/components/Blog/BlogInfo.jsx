54a749df03880a9e8b7cd8e0b467d515fa06973a
John Doe <johndoe@example.com>
2024-08-17 16:49:49 +0500

    more additions on blog page

--- react-project/src/components/Blog/BlogInfo.jsx
+++ react-project/src/components/Blog/BlogInfo.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import blogImage from '../../assets/images/blog-1.png'; 
 
 export default function BlogInfo(props) {
   return (
     <div className="blog-info-wrapper">
-        <p>{props.author}</p>
+        <p>{props.infos.author}</p>
+        <img src={blogImage} alt="" />
     </div>
   )
 }
--- react-project/src/components/Blog/index.jsx
+++ react-project/src/components/Blog/index.jsx
@@ -1,21 +1,28 @@
 import './style.scss';
 import blogs from '../../db/blog.json';
-import blogImage from '../../assets/images/blog-1.png';
 import { useState } from 'react';
 import BlogInfo from './BlogInfo';
 
 
 function Blog(props) {
-    const [information, setInformation] = useState("")
+    const [information, setInformation] = useState(null)
     function getInfo() {
         for(let item of blogs){
-            
+            setInformation(item)
+            information?.map((infos,index) => {
+                console.log(infos)
+                return (
+                    <div key={index} className="more-info">
+                        <BlogInfo infos={infos}/>
+                    </div>
+                )
+            })
            
         }
     }
     return (
         <main className="blog-page-wrapper">
-          <BlogInfo info={getInfo()}/>
+          <BlogInfo />
         </main>
     );
 }
