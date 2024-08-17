cffc9d54cef2909e81bc7398bf9926e6f3623429
John Doe <johndoe@example.com>
2024-08-17 16:55:20 +0500

    some changes

--- react-project/src/components/Blog/BlogInfo.jsx
+++ react-project/src/components/Blog/BlogInfo.jsx
@@ -4,7 +4,7 @@ import blogImage from '../../assets/images/blog-1.png';
 export default function BlogInfo(props) {
   return (
     <div className="blog-info-wrapper">
-        <p>{props.infos.author}</p>
+        <p>{props.author}</p>
         <img src={blogImage} alt="" />
     </div>
   )
--- react-project/src/components/Blog/index.jsx
+++ react-project/src/components/Blog/index.jsx
@@ -4,19 +4,11 @@ import { useState } from 'react';
 import BlogInfo from './BlogInfo';
 
 
-function Blog(props) {
-    const [information, setInformation] = useState(null)
+function Blog() {
+    const [information, setInformation] = useState("")
     function getInfo() {
         for(let item of blogs){
-            setInformation(item)
-            information?.map((infos,index) => {
-                console.log(infos)
-                return (
-                    <div key={index} className="more-info">
-                        <BlogInfo infos={infos}/>
-                    </div>
-                )
-            })
+            
            
         }
     }
