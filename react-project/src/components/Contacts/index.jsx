3b76a3509e5704d308664d90b5d34204a6ff8bac
Alisher <alisherxujanov163@gmail.com>
2024-08-24 18:28:35 +0500

    inputs

--- react-project/src/components/Contacts/index.jsx
+++ react-project/src/components/Contacts/index.jsx
@@ -1,62 +1,106 @@
-import Heading from "../common/Heading";
-import "./style.scss";
-import mapImage from '../../assets/images/contact/map.png';
-import secondMap from '../../assets/images/contact/map1.png';
+import "./style.scss"
+import Heading from "../common/Heading"
+import mapImage from '../../assets/images/contact/map.png'
+import secondMap from '../../assets/images/contact/map1.png'
+import { useState } from 'react'
 
+
+// RULE
+// 1. Import any hook from 'react'
+//    ex:   import { useState } from 'react';
+
+// 2. Hooks must be called in the component body first of all meaning that
+//    you should call it before anything else
+
+// 3. useState:  when using useState hook, you get two params from it
+//    - first:  variable that is used as memory of component
+//    - function:  function that is used to update the variable
+//                 You must use only this function but not other ones!
 
 function Contacts() {
-  return (
-    <main className="contacts-page-wrapper">
-      
-      <div className="left">
-      <Heading size={1.2}>Контакты</Heading>
-        <div>
-          <h5>Задайте нам любой вопрос</h5>
-          <p>+7 701 776 24 20</p>
-        </div>
-        <div>
-          <h5>Электронная почта</h5>
-          <p>client@fonte.kz</p>
-        </div>
-        <div>
-          <h5>Юридический адрес</h5>
-          <p style={{width: '400px'}}>
-            050040/A15E3H4, проспект Аль-Фараби, 77/7​, 10 этаж, Алматы,
-            Казахстан Z05T3D0, проспект Мангилик Ел, 55/20, Офисы 345-346,
-            Астана, Казахстан
-          </p>
-        </div>
-        <div className="bottom">
-        
-        <div>
-        <img src={secondMap}  />
-        </div>
-    <div>
-    <img src={mapImage}  />
-    </div>
-    
-        
-    
-  </div>
-      </div>
-      
-      <div className="rigth">
-        <form >
-            <div>
-                <input type="text" placeholder="Name"/>
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        phone: "+998",
+    })
+
+
+    function setInputValueIntoState(e) {
+        // let name = e.target.name
+        // let value = e.target.value
+        let { name, value } = e.target
+        setState({ ...state, [name]: value })
+    }
+
+
+    function submit(e) { }
+
+    return (
+        <main className="contacts-page-wrapper">
+
+            <div className="left">
+                <Heading size={1.2}>Контакты</Heading>
+                <div>
+                    <h5>Задайте нам любой вопрос</h5>
+                    <p>+7 701 776 24 20</p>
+                </div>
+                <div>
+                    <h5>Электронная почта</h5>
+                    <p>client@fonte.kz</p>
+                </div>
+                <div>
+                    <h5>Юридический адрес</h5>
+                    <p style={{ width: '400px' }}>
+                        050040/A15E3H4, проспект Аль-Фараби, 77/7​, 10 этаж, Алматы,
+                        Казахстан Z05T3D0, проспект Мангилик Ел, 55/20, Офисы 345-346,
+                        Астана, Казахстан
+                    </p>
+                </div>
+                <div className="bottom">
+                    <div>
+                        <img src={secondMap} />
+                    </div>
+                    <div>
+                        <img src={mapImage} />
+                    </div>
+                </div>
             </div>
-            <div>
-                <input type="text" placeholder="Email"/>
+
+            <div className="rigth">
+                <b>{state.name}</b> <br />
+                <b>{state.email}</b> <br />
+                <b>{state.phone}</b><br />
+
+                <form onSubmit={submit}>
+                    <div>
+                        <input
+                            onChange={setInputValueIntoState}
+                            type="text" placeholder="Name" name="name"
+                            value={state.name}
+                            pattern="^[a-zA-Z_ ]+$"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            onChange={setInputValueIntoState}
+                            type="email" placeholder="Email" name="email"
+                            value={state.email}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            onChange={setInputValueIntoState}
+                            type="tel" placeholder="Phone" name="phone"
+                            value={state.phone}
+                            pattern="^\+998[0-9]{9}$"
+                        />
+                    </div>
+                    <button className="warning-btn">Отправить</button>
+                </form>
             </div>
-            <div>
-                <input type="text" placeholder="Phone"/>
-            </div>
-            <button className="warning-btn">Отправить</button>
-        </form>
-      </div>
-      
-    </main>
-  );
+
+        </main>
+    );
 }
 
 export default Contacts;
