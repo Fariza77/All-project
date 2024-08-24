1aa31ae290bcd94eccbc87a7bfd93e098b50a07e
Alisher <alisherxujanov163@gmail.com>
2024-08-24 08:54:48 +0500

    navbar

--- react-project/src/components/Navigation/index.jsx
+++ react-project/src/components/Navigation/index.jsx
@@ -1,16 +1,38 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar'
+import Footer from "./Footer"
+import './style.scss'
 
 function Navigation() {
+    const [burgerMenuOpened, setBurgerMenuOpened] = useState(false)
+
     return (
-        <>
-            <Navbar />
+        <div>
+            <header>
+                <div className={burgerMenuOpened ? "burger-menu-wrapper opened" : "burger-menu-wrapper"}>
+                    <div className='top'></div>
+                    <div className='middle'></div>
+                    <div className='bottom'></div>
+                </div>
+                <input
+                    id='burger-menu-toggler'
+                    type="checkbox"
+                    onClick={(e) => { setBurgerMenuOpened(e.target.checked) }}
+                />
+                {/* ...:checked + .nav-wrapper > .menu */}
+                <Navbar />
+            </header>
 
-            <Outlet /> 
+            <div id='outlet'>
+                <Outlet />
+            </div>
 
-            <Footer />
-        </>
+            <footer id="footer">
+                <Footer />
+            </footer>
+        </div>
+
     );
 }
 
