ee009a20ab0f5f416be2bc2e05a25d1d59253e69
John Doe <johndoe@example.com>
2024-08-29 15:40:49 +0500

    connected to firebase database

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AllComponents from "./components/AllComponents.jsx"
+import './firebase.js'
 
 export default function App() {
 
