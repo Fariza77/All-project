556a68d88d9b80a95f7b1d4a724897c088d7a0fd
Alisher <alisherxujanov163@gmail.com>
2024-10-15 08:03:03 +0500

    MD files

---
+++ react-project/src/components/FetchUser.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { globalContext } from '../store'
+
+// HOC -> Higher Order Component 
+// is used to control the access to a component
+
+export default function fetchUser(Component) {
+    return function AuthenticatedComponent(props) {
+        const state = useContext(globalContext)
+        const navigate = useNavigate()
+
+        useEffect(() => {
+            // if (localStorage.getItem("logged-in-user") == null) {
+            //     sendToAuth()
+            //     console.clear()
+            //     return
+            // }    
+
+            let user = localStorage.getItem("logged-in-user")
+            if (user != null) {
+                state.dispatch({ type: "SET_USER", payload: user })
+            }
+        }, [navigate])
+
+        return <Component {...props} />
+    }
+}
