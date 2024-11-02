78c315b529ac6db386740be4b4946053f613bf24
Alisher <alisherxujanov163@gmail.com>
2024-11-02 17:44:20 +0500

    translated about page

--- react-project/src/components/About/index.jsx
+++ react-project/src/components/About/index.jsx
@@ -6,10 +6,12 @@ import { useEffect } from 'react'
 import Lycence from "../../assets/images/certificates/second.png"
 import { useContext } from "react"
 import { globalContext } from "../../store/index.js"
+import { useTranslation } from "react-i18next";
 
 
 function About() {
   const state = useContext(globalContext)
+  const { t } = useTranslation();
 
   useEffect(() => {
     document.title = "About Us";
@@ -17,17 +19,17 @@ function About() {
 
   return (
     <main className="about-page-wrapper">
-      <Heading size={1.4}>О нас</Heading>
+      <Heading size={1.4}>{t("about")}</Heading>
 
       <div className="content-wrapper">
         <img src={ImageAbout} alt="ImageAbout" width={"100%"} height={450} />
 
         <div className="info">
-          <p>Наша Команда успешно осуществляет деятельность на нескольких рынках инвестиций. Богатство выбора инструментов этих рынков позволяет Нам успешно сохранять и преумножать капитал клиентов. </p>
-          <p>Вступить в партнерство с Fonte могут как профессиональные инвестора, так и частные лица, только начинающие открывать для себя новые перспективы. Наша юрисдикция - Международный финансовый центр «Астана» (МФЦА). Комфортные налоговые условия и регуляторные политики обеспечивают необходимые свободы и преимущества для достижения целей инвестиций. </p>
-          <p>В партнерстве с Fonte Capital LTD, инвесторы имеют возможность воспользоваться не только проверенными стратегиями, но и смогут совместно разработать персональные инвестиционные решения.</p>
-          <a className="warning-btn" href={Lycence} download={"Lycence"} style={{ display: "inline-block"}}>
-            <TbLicense /> Лицензии
+          <p>{t("about-page.team-info")}</p>
+          <p>{t("about-page.team-info2")}</p>
+          <p>{t("about-page.team-info3")}</p>
+          <a className="warning-btn" href={Lycence} download={"Lycence"} style={{ display: "inline-block" }}>
+            <TbLicense /> {t("about-page.licences")}
           </a>
 
         </div>
