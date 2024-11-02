92fd190a7fb4028482be7ad21e8a9fe63a9c17ca
Alisher <alisherxujanov163@gmail.com>
2024-11-02 17:02:33 +0500

    installed emailJS

--- react-project/src/components/Navigation/Footer/Consultation.jsx
+++ react-project/src/components/Navigation/Footer/Consultation.jsx
@@ -1,34 +1,79 @@
 import { useState } from 'react'
 import "./consultation.scss"
 import Wallpaper from "../../../assets/images/consultation-wallpaper.png"
+import { toast } from 'react-toastify'
+import emailjs from '@emailjs/browser';
 
 function Consultation(props) {
     const [consultationForm, setConsultationForm] = useState({
-        name: '',
+        client_name: '',
         email: '',
         phone: '',
         message: ''
     })
     function submit(e) {
         e.preventDefault()
+        if (!consultationForm.client_name || !consultationForm.message) {
+            toast.error('Please fill in all the fields', { theme: "dark" })
+            return
+        }
+        let templateParams = {
+            client_name: consultationForm.client_name,
+            message: consultationForm.message
+        }
+
+        emailjs.send('service_8aytxhp', 'template_7tolh3p', templateParams, {
+            publicKey: 'mbcCG18ZiPltCRfB-',
+        })
+            .then(
+                (response) => {
+                    toast.success('SUCCESS!', { theme: "dark" })
+                },
+                (err) => {
+                    toast.error('FAILED! ' + err, { theme: "dark" })
+                },
+            )
+        setConsultationForm({
+            client_name: '',
+            email: '',
+            phone: '',
+            message: ''
+        })
+    }
+
+    function handleFormData(e) {
+        let { name, value } = e.target
+        setConsultationForm({ ...consultationForm, [name]: value })
     }
 
     return (
         <div className="consultation-wrapper">
             <form onSubmit={submit}>
                 <div className="form-control">
-                    <input id='consultation-person-fullname' type="text" name="fullname" placeholder='Full name' />
+                    <input id='consultation-person-fullname' type="text" name="client_name" placeholder='Full name'
+                        onChange={handleFormData}
+                        value={consultationForm.client_name}
+                    />
                 </div>
                 <div className="form-control row">
                     <div className="left">
-                        <input id='consultation-person-email' type="email" name="email" placeholder='Email' />
+                        <input id='consultation-person-email' type="email" name="email" placeholder='Email'
+                            onChange={handleFormData}
+                            value={consultationForm.email}
+                        />
                     </div>
                     <div className="right">
-                        <input id='consultation-person-phone' type="tel" name="phone" placeholder='Phone' />
+                        <input id='consultation-person-phone' type="tel" name="phone" placeholder='Phone'
+                            onChange={handleFormData}
+                            value={consultationForm.phone}
+                        />
                     </div>
                 </div>
                 <div className="form-control">
-                    <textarea id="consultation-person-message" name="message" placeholder="Message" />
+                    <textarea id="consultation-person-message" name="message" placeholder="Message"
+                        onChange={handleFormData}
+                        value={consultationForm.message}
+                    />
                 </div>
                 <div className="form-control">
                     <button className='warning-btn'>Получить консультацию</button>
